19c3fd56b4ef5367338830b6e2cd1024347a22a2
Mo Jiwa <mohammed.jiwa@gmail.com>
2021-03-13 20:06:04 +0000

    build and update

--- backend/build/index.js
+++ backend/build/index.js
@@ -4,9 +4,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_graphql_1 = require("express-graphql");
+const graphql_1 = require("graphql");
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
 const port = 8000;
-app.get('/', (req, res) => res.send("Express Typescript Server"));
+app.use(cors_1.default());
+// the schema defines the queries and what parameters they will take
+let schema = graphql_1.buildSchema(`
+    type Query { 
+        hello: String,
+        goodbye: String,
+        add(numberOne: Int!, numberTwo: Int!): Int,
+        getList: [String],
+    }
+`);
+function goodbye() {
+    return "This is a goodbye";
+}
+function add(number1, number2) {
+    return number1 + number2;
+}
+function getList() {
+    return ["Item 1", "Item 2", "Item 3", "Item 4"];
+}
+// when the query is made, state what is returned
+let root = {
+    hello: () => {
+        return "Hello world!";
+    },
+    goodbye: () => goodbye(),
+    add: (args) => add(args.numberOne, args.numberTwo),
+    getList: () => getList(),
+};
+// all api requests are made to a single graphql endpoint
+app.use(`/graphql`, express_graphql_1.graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- backend/index.ts
+++ backend/index.ts
@@ -48,5 +48,5 @@ app.use(`/graphql`, graphqlHTTP({
 }));
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);    
+    console.log(`[server]: Server is running at http://localhost:${port}`);    
 });
